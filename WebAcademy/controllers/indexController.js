0c42f642ab6e78a2875e56ba217afbc7878c1806
brunozapico <brunoezapico@gmail.com>
2020-05-17 16:21:55 -0300

    Agregue el partial scripts

--- WebAcademy/controllers/indexController.js
+++ WebAcademy/controllers/indexController.js
@@ -2,7 +2,7 @@ const categories = require('../data/categoriesDataBase.json');
 const tips = require('../data/tipsDataBase.json');
 
 const indexController = {
-    index: function(req, res, next) {
+    index: function(req, res) {
         res.render('index', {categories, tips});
     }
 }
