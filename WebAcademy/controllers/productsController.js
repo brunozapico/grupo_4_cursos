d202dc556b2e437b3a675d6cdcead495d3f077ea
Lucaster85 <lucasdc87@gmail.com>
2020-05-21 17:33:07 -0300

    agregar la logica de delete y edit

--- WebAcademy/controllers/productsController.js
+++ WebAcademy/controllers/productsController.js
@@ -101,7 +101,49 @@ const productsController = {
         res.render('productEdit', {product});
     },
     update: (req, res, next) => {
+        let product = {
+            category: req.body.category,
+            category_image: req.files[0].filename,
+            days: req.body.days,
+            description: req.body.description,
+            description_short: req.body.description_short,
+            end: `${req.body.end.slice(-2)}-${req.body.end.slice(5,7)}-${req.body.end.slice(0,4)}`,
+            id: Date.now(),
+            image: req.files[1].filename,
+            name: req.body.name,
+            outstanding: req.body.outstanding,
+            price: req.body.price,
+            start: `${req.body.start.slice(-2)}-${req.body.start.slice(5,7)}-${req.body.start.slice(0,4)}`,
+            time: req.body.time,
+            vacancies: req.body.vacancies
+        };
+        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), {encoding: 'UTF-8'}))
+
+        let id = products.findIndex(product => {
+            return product.id == req.params.id;
+        })
+
+        products[id] = product;
+
+        productsDB = JSON.stringify(products);
+
+        fs.writeFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), productsDB);
+
+        res.redirect('/products');
         
+    },
+    destroy : (req, res) => {
+        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), {encoding: 'UTF-8'}))
+        
+        products = products.filter(product => {
+            return product.id != req.params.id;
+        })
+
+        productsDB = JSON.stringify(products);
+
+        fs.writeFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), productsDB);
+
+        res.send('ELIMINADO')
     }
 }
 
