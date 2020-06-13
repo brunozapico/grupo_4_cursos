a6af5c253e348af530a7f8b32ad3bf690e556506
brunozapico <brunoezapico@gmail.com>
2020-06-13 20:33:26 -0300

    Modelo de categories

---
+++ WebAcademy/database/models/Category.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = 'Category';
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        title: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+            unique: true
+        },
+        icon: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        avatar: {
+            type: dataTypes.STRING(255),
+            allowNull: false,
+        }
+    };
+
+    let config = {
+        tableName:'categories',
+        underscored: true,
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    };
+    
+    const Category = sequelize.define(alias, cols, config);
+
+    Category.associate = models => {
+        Category.hasMany(models.Course, {
+            as: 'courses',
+            foreignKey: 'category_id'
+        });
+    };
+
+    return Category;
+};
