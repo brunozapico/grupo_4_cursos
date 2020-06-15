8b1d87e5ccf9123454925def98a6f32d8c772a73
Lucaster85 <lucasdc87@gmail.com>
2020-06-15 16:06:00 -0300

    en proceso del metodo create y update del productController

--- WebAcademy/controllers/productsController.js
+++ WebAcademy/controllers/productsController.js
@@ -13,43 +13,28 @@ const productsController = {
         });
     },
     create: (req, res, next) => {
-        res.render('productForm', { loggedInUser: req.session.loggedIn })
+        db.Category.findAll({ //tengo que seguir pasandole todos los datos para la navegacion
+            include: { associations: 'course', associations: 'program' }
+        }).then(categories => {
+            res.render('productsForm', { categories, title: 'Carga tu curso', loggedInUser: req.session.loggedIn })
+        });
     },
     store: (req, res, next) => {
-
-        let product = {
-            category: req.body.category,
-            category_image: req.files[0].filename,
-            days: req.body.days,
-            description: req.body.description,
-            description_short: req.body.description_short,
-            end: `${req.body.end.slice(-2)}-${req.body.end.slice(5, 7)}-${req.body.end.slice(0, 4)}`,
-            id: Date.now(),
-            image: req.files[1].filename,
+        db.Course.create({
             name: req.body.name,
-            outstanding: req.body.outstanding,
             price: req.body.price,
-            start: `${req.body.start.slice(-2)}-${req.body.start.slice(5, 7)}-${req.body.start.slice(0, 4)}`,
-            time: req.body.time,
-            vacancies: req.body.vacancies
-        }
-
-        let productsDB = fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), { encoding: 'UTF-8' })
-
-        let products;
-        if (productsDB == "") {
-            products = []
-        } else {
-            products = JSON.parse(productsDB);
-        }
-
-        products.push(product);
-
-        productsDB = JSON.stringify(products);
-
-        fs.writeFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), productsDB);
-
-        res.redirect('/products');
+            starts_date: req.body, //completar
+            ends_date: req.body, //completar
+            image: req.body, //completar
+            vacancies: req.body.vacancies,
+            outstanding: req.body.outstanding,
+            description_short: req.body.description_short,
+            description_full: req.body.description_full,
+            category_id: req.body.categoty,
+            professor_id: req.body.professor
+        }).then(() => {
+            res.redirect('/products')
+        });
     },
     detail: (req, res) => {
         let id = products.findIndex(product => {
