63796319d466d3afaf496908a36521ac743c362f
MartinRodriguez1995 <Martinn.rodriguez@outlook.com>
2020-06-15 15:58:26 -0300

    Hice Detail, Edit, Update y Destroy del controlador de productos

--- WebAcademy/controllers/productsController.js
+++ WebAcademy/controllers/productsController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = require('../database/models');
+const db = require('../database/models');
 const Op = db.Sequelize.Op;
 
 const productsController = {
@@ -58,68 +58,52 @@ const productsController = {
         res.redirect('/products');
     },
     detail: (req, res) => {
-        let id = products.findIndex(product => {
-            return product.id == req.params.id
-        });
+        db.Course.findByPk(req.params.id, {
+            includ: [{association : 'professors'}, {association : 'program'}, {association : 'categories'}]
+        })
+            .then(course =>{
+                res.render('productDetail', {course, loggedInUser: req.session.loggedIn});
+            });
         
-        let product = products[id];
-        
-        res.render('productDetail', {product, loggedInUser: req.session.loggedIn});
     },
     edit: (req, res, next) => {
-        let id = products.findIndex(product => {
-            return product.id == req.params.id
-        });
-        
-        let product = products[id];
-        
-        res.render('productEdit', {product, loggedInUser: req.session.loggedIn});
+        let courseEdit = db.Course.findByPk(req.params.id);
+        let categoryEdit = db.Category.findAll();
+
+            Promise.all([courseEdit, categoryEdit])
+            .then(([Course, Category]) => {
+                res.render('productEdit', {Course:Course, Category:Category, loggedInUser: req.session.loggedIn});
+            });
     },
     update: (req, res, next) => {
-        let product = {
-            category: req.body.category,
-            category_image: req.files[0].filename,
-            days: req.body.days,
-            description: req.body.description,
-            description_short: req.body.description_short,
-            end: `${req.body.end.slice(-2)}-${req.body.end.slice(5,7)}-${req.body.end.slice(0,4)}`,
-            id: Date.now(),
-            image: req.files[1].filename,
-            name: req.body.name,
-            outstanding: req.body.outstanding,
-            price: req.body.price,
-            start: `${req.body.start.slice(-2)}-${req.body.start.slice(5,7)}-${req.body.start.slice(0,4)}`,
-            time: req.body.time,
-            vacancies: req.body.vacancies
-        };
-        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), {encoding: 'UTF-8'}))
-
-        let id = products.findIndex(product => {
-            return product.id == req.params.id;
-        })
-
-        products[id] = product;
-
-        productsDB = JSON.stringify(products);
-
-        fs.writeFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), productsDB);
-
-        res.redirect('/products');
-        
+            db.Course.update({
+                name: req.body.name,
+                price: req.body.price,
+                starts_date: req.body.starts_date,
+                ends_date: req.body.ends_date,
+                image: req.body.image,
+                vacancies: req.body.vacancies,
+                outstanding: req.body.outstanding,
+                description_short: req.body.description_short,
+                description_full: req.body.description_full
+            },{
+                where :{
+                    id : req.params.id}
+            })
+            .then( () =>{
+                res.redirect('/products')
+            });
     },
     destroy : (req, res) => {
-        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), {encoding: 'UTF-8'}))
-        
-        products = products.filter(product => {
-            return product.id != req.params.id;
-        })
-
-        productsDB = JSON.stringify(products);
-
-        fs.writeFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), productsDB);
-
-        res.redirect('/products');
-    }
+        db.Course.destroy({
+            where : {
+                id :req.params.id
+                }
+            }
+            .then(()=>{
+                res.redirect('/products')
+            })
+    )}
 }
 
 
