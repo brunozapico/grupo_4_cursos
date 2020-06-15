0facb582d90b87d1bf21d061e6f3dd7850985130
Lucaster85 <lucasdc87@gmail.com>
2020-06-15 13:33:12 -0300

    modifico el metodo list del productController para trabajar con DB

--- WebAcademy/controllers/productsController.js
+++ WebAcademy/controllers/productsController.js
@@ -1,58 +1,52 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = require('../database/models');
+const db = require('../database/models');
 const Op = db.Sequelize.Op;
 
 const productsController = {
     list: (req, res) => {
-        res.render('products', {
-            artProducts,
-            developmentProducts,
-            personalDevelopmentProducts,
-            designProducts,
-            photografyProducts,
-            computingProducts,
-            marketingProducts,
-            businessProducts,
-            loggedInUser: req.session.loggedIn
+        db.Category.findAll({
+            include: { associations: 'course' }
+        }).then(categories => {
+            res.render('products', { categories, title: 'Todos nuestros cursos', loggedInUser: req.session.loggedIn })
         });
     },
-    create: (req, res, next) =>{
-        res.render('productForm', {loggedInUser: req.session.loggedIn})
+    create: (req, res, next) => {
+        res.render('productForm', { loggedInUser: req.session.loggedIn })
     },
-    store: (req, res, next) =>{
-        
+    store: (req, res, next) => {
+
         let product = {
             category: req.body.category,
             category_image: req.files[0].filename,
             days: req.body.days,
             description: req.body.description,
             description_short: req.body.description_short,
-            end: `${req.body.end.slice(-2)}-${req.body.end.slice(5,7)}-${req.body.end.slice(0,4)}`,
+            end: `${req.body.end.slice(-2)}-${req.body.end.slice(5, 7)}-${req.body.end.slice(0, 4)}`,
             id: Date.now(),
             image: req.files[1].filename,
             name: req.body.name,
             outstanding: req.body.outstanding,
             price: req.body.price,
-            start: `${req.body.start.slice(-2)}-${req.body.start.slice(5,7)}-${req.body.start.slice(0,4)}`,
+            start: `${req.body.start.slice(-2)}-${req.body.start.slice(5, 7)}-${req.body.start.slice(0, 4)}`,
             time: req.body.time,
             vacancies: req.body.vacancies
         }
-        
-        let productsDB = fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json') , {encoding: 'UTF-8'})
-        
+
+        let productsDB = fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), { encoding: 'UTF-8' })
+
         let products;
-        if (productsDB == ""){
+        if (productsDB == "") {
             products = []
         } else {
             products = JSON.parse(productsDB);
         }
-        
+
         products.push(product);
-        
+
         productsDB = JSON.stringify(products);
-        
+
         fs.writeFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), productsDB);
 
         res.redirect('/products');
@@ -61,19 +55,19 @@ const productsController = {
         let id = products.findIndex(product => {
             return product.id == req.params.id
         });
-        
+
         let product = products[id];
-        
-        res.render('productDetail', {product, loggedInUser: req.session.loggedIn});
+
+        res.render('productDetail', { product, loggedInUser: req.session.loggedIn });
     },
     edit: (req, res, next) => {
         let id = products.findIndex(product => {
             return product.id == req.params.id
         });
-        
+
         let product = products[id];
-        
-        res.render('productEdit', {product, loggedInUser: req.session.loggedIn});
+
+        res.render('productEdit', { product, loggedInUser: req.session.loggedIn });
     },
     update: (req, res, next) => {
         let product = {
@@ -82,17 +76,17 @@ const productsController = {
             days: req.body.days,
             description: req.body.description,
             description_short: req.body.description_short,
-            end: `${req.body.end.slice(-2)}-${req.body.end.slice(5,7)}-${req.body.end.slice(0,4)}`,
+            end: `${req.body.end.slice(-2)}-${req.body.end.slice(5, 7)}-${req.body.end.slice(0, 4)}`,
             id: Date.now(),
             image: req.files[1].filename,
             name: req.body.name,
             outstanding: req.body.outstanding,
             price: req.body.price,
-            start: `${req.body.start.slice(-2)}-${req.body.start.slice(5,7)}-${req.body.start.slice(0,4)}`,
+            start: `${req.body.start.slice(-2)}-${req.body.start.slice(5, 7)}-${req.body.start.slice(0, 4)}`,
             time: req.body.time,
             vacancies: req.body.vacancies
         };
-        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), {encoding: 'UTF-8'}))
+        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), { encoding: 'UTF-8' }))
 
         let id = products.findIndex(product => {
             return product.id == req.params.id;
@@ -105,11 +99,11 @@ const productsController = {
         fs.writeFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), productsDB);
 
         res.redirect('/products');
-        
+
     },
-    destroy : (req, res) => {
-        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), {encoding: 'UTF-8'}))
-        
+    destroy: (req, res) => {
+        let products = JSON.parse(fs.readFileSync(path.join(__dirname + '/../' + 'data/' + 'products.json'), { encoding: 'UTF-8' }))
+
         products = products.filter(product => {
             return product.id != req.params.id;
         })
