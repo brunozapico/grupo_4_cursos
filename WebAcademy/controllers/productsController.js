2c593045ea7456fcbc57c4f9f781b696aec1ee4b
Lucaster85 <lucasdc87@gmail.com>
2020-06-15 19:01:07 -0300

    agrego el metodo search al controller y hago algunas correcciones en el mismo

--- WebAcademy/controllers/productsController.js
+++ WebAcademy/controllers/productsController.js
@@ -25,7 +25,7 @@ const productsController = {
             price: req.body.price,
             starts_date: req.body.starts_date,
             ends_date: req.body.ends_date,
-            image: req.body, //completar
+            image: `/img/cursos/${req.files[0].filename}`,
             vacancies: req.body.vacancies,
             outstanding: req.body.outstanding,
             description_short: req.body.description_short,
@@ -50,8 +50,8 @@ const productsController = {
         let categoryEdit = db.Category.findAll();
 
             Promise.all([courseEdit, categoryEdit])
-            .then(([Course, Category]) => {
-                res.render('productEdit', {Course:Course, Category:Category, loggedInUser: req.session.loggedIn});
+            .then(([courses, categories]) => {
+                res.render('productEdit', {courses, categories, loggedInUser: req.session.loggedIn});
             });
     },
     update: (req, res, next) => {
@@ -60,17 +60,19 @@ const productsController = {
                 price: req.body.price,
                 starts_date: req.body.starts_date,
                 ends_date: req.body.ends_date,
-                image: req.body.image,
+                image: `/img/cursos/${req.files[0].filename}`,
                 vacancies: req.body.vacancies,
                 outstanding: req.body.outstanding,
                 description_short: req.body.description_short,
-                description_full: req.body.description_full
+                description_full: req.body.description_full,
+                category_id: req.body.categoty,
+                professor_id: req.body.professor
             },{
                 where :{
                     id : req.params.id}
             })
             .then( () =>{
-                res.redirect('/products')
+                res.redirect(`/products/detail/${req.params.id}`)
             });
     },
     destroy : (req, res) => {
@@ -82,7 +84,26 @@ const productsController = {
             .then(()=>{
                 res.redirect('/products')
             })
-    )}
+    )},
+    search(req,res) {
+        let resultadoBusqueda  = db.Course.findAll({
+            where: {
+                name: {
+                    [Op.substring]: req.query.q,
+                }
+            },
+            include: [{association: 'categories'}]
+        })
+        let categories = db.Category.findAll({
+            include: { associations: 'course' }
+        })
+
+        Promise.all([resultadoBusqueda, categories])
+
+        .then(([resultadoBusqueda, categories]) => {
+            res.render('search', {resultadoBusqueda, categories, title: 'Este es el resultado de tu busqueda'})
+        })
+    }
 }
 
 
