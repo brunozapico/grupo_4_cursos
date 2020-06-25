74d0e449fb41c9eb96080b72994fe4c046cf72b6
brunozapico <brunoezapico@gmail.com>
2020-06-25 11:52:39 -0300

    Mejoras en UX front validation

--- WebAcademy/public/js/registerValidator.js
+++ WebAcademy/public/js/registerValidator.js
@@ -3,15 +3,19 @@ window.addEventListener('load', () => {
 
         name =  form.name,
         name_div = document.getElementById('name-div'),
+        name_error_log = document.getElementById('name-error-log');
 
         email = form.email,
-        email_div = document.getElementById('email-div'),  
+        email_div = document.getElementById('email-div'),
+        email_error_log = document.getElementById('email-error-log');
 
         password = form.password,
         password_div = document.getElementById('password-div'),
+        password_error_log = document.getElementById('password-error-log');
          
         confirmPassword = form.c_password,
         c_password_div = document.getElementById('c-password-div'),
+        c_password_error_log = document.getElementById('c-password-error-log');
 
         avatar = form.avatar,
 
@@ -26,41 +30,53 @@ window.addEventListener('load', () => {
         };
     };
 
-    valid_check = input => {
-        if(input.classList.contains('invalid')){
-            input.classList.replace('invalid', 'valid');
+    valid_check = (div, error_log) => {
+        if(div.classList.contains('invalid')){
+            div.classList.replace('invalid', 'valid');
         } else {
-            input.classList.add('valid');
+            div.classList.add('valid');
         };
-    }
 
-    invalid_check = input => {
-        if(input.classList.contains('valid')){
-            input.classList.replace('valid', 'invalid');
+        if(error_log.classList.contains('visible')){
+            error_log.classList.replace('visible', 'hidden');
         } else {
-            input.classList.add('invalid');
+            error_log.classList.add('hidden');
         };
-    }
+    };
+
+    invalid_check = (div, error_log) => {
+        if(div.classList.contains('valid')){
+            div.classList.replace('valid', 'invalid');
+        } else {
+            div.classList.add('invalid');
+        };
+
+        if(error_log.classList.contains('hidden')){
+            error_log.classList.replace('hidden', 'visible');
+        } else {
+            error_log.classList.add('visible');
+        };
+    };
 
     password_check = () => {
         if(password.value.length >= 8 && password.value === confirmPassword.value){
-            valid_check(password_div);
-            valid_check(c_password_div);
+            valid_check(password_div, password_error_log);
+            valid_check(c_password_div, c_password_error_log);
         } else {
-            invalid_check(password_div);
-            invalid_check(c_password_div);
+            invalid_check(password_div, password_error_log);
+            invalid_check(c_password_div, c_password_error_log);
         };
 
         enable();
-    }
+    };
 
     // NAME
     name.addEventListener('input', event => {
                 
         if(name.value.length >= 2){
-            valid_check(name_div);
+            valid_check(name_div, name_error_log);
         } else {
-            invalid_check(name_div);
+            invalid_check(name_div, name_error_log);
         };
 
         enable();
@@ -69,9 +85,9 @@ window.addEventListener('load', () => {
     // EMAIL
     email.addEventListener('input', event => {
         if (email.validity.typeMismatch || email.value.length == 0) {
-            invalid_check(email_div);
+            invalid_check(email_div, email_error_log);
         } else {
-            valid_check(email_div);
+            valid_check(email_div, email_error_log);
         };
 
         enable();
