b15dacbb6cbdbd133bfda736046102d297df36e5
brunozapico <brunoezapico@gmail.com>
2020-06-25 14:42:05 -0300

    Back validation 100%

--- WebAcademy/middlewares/loginValidator.js
+++ WebAcademy/middlewares/loginValidator.js
@@ -1,45 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const bcrypt = require('bcrypt');
-const { check, validationResult, body } = require('express-validator');
-const db = require('../database/models');
+const { check } = require('express-validator');
 
 module.exports = [
     check('email')
     .isEmail()
     .withMessage('Ingrese una dirección de correo electrónico válida.'),
-    
-    // SOLUCIONAR ESTA VALIDACION
-    body('email')
-    .custom( (value, {req}) => {
-        if(value != ''){
-            db.User.findOne({where: {email: req.body.email}})
-            .then(user => {
-                if (user != null){
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-        } else {
-            return true; // se agarra el error con el isEmail(), si no se repite.
-        }
-    })
-    .withMessage('Ingrese una dirección de correo electrónico válida.'),
-    
-    // SOLUCIONAR ESTA VALIDACION
-    body('password')
-    .custom( (value, {req}) => {
-        let check;
-        db.User.findOne({where: {email: req.body.email}})
-        .then(user => {
-            if(user != null) {
-                check = bcrypt.compareSync(req.body.password, user.password);
-                return check;
-            } else {
-                return false;
-            }
-        })
-    })
-    .withMessage('Contraseña incorrecta.')
-]
+];
--- WebAcademy/middlewares/registerValidator.js
+++ WebAcademy/middlewares/registerValidator.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
 const path = require('path');
-const bcrypt = require('bcrypt');
-const { check, validationResult, body } = require('express-validator');
-const db = require('../database/models');
+const { check, body } = require('express-validator');
 
 module.exports = [
     check('name')
@@ -13,28 +10,11 @@ module.exports = [
     .isEmail()
     .withMessage('Debes ingresar una dirección de correo electrónico válida.'),
     
-    // SOLUCIONAR ESTA VALIDACION
-    body('email')
-    .custom( (value, {req}) => {
-        if(value != '') {
-            db.User.findOne({where: {email: req.body.email}})
-            .then(user => {
-                if(user != null){
-                    return false;
-                }
-                return true;
-            })
-        } else {
-            return true; // se agarra el error con el isEmail(), si no se repite.
-        }
-    })
-    .withMessage('Esa dirección de correo electrónico ya existe.'),
-    
     check('password')
     .isLength({min:8})
     .withMessage('La contraseña debe tener al menos 8 carácteres.'),
 
-    body('c-password')
+    body('c_password')
     .custom((value, {req}) => {
         if(value != req.body.password) {
             return false
@@ -64,4 +44,4 @@ module.exports = [
         }
     })
     .withMessage('El archivo debe ser de formato: ".jpg", ".jpeg", ".png" o ".gif".')
-]
+];
