291adc4a2a9af55f214efa4795148367f027acea
Lucaster85 <lucasdc87@gmail.com>
2020-06-26 13:46:56 -0300

    arreglo la validacion del back para que no envie el form sin una imagen cargada

--- WebAcademy/middlewares/productFormValidator.js
+++ WebAcademy/middlewares/productFormValidator.js
@@ -82,8 +82,8 @@ module.exports = [
                     return false;
                 }
             } else {
-                return true;
+                return false;
             }
     })
-    .withMessage('El archivo debe ser de formato: ".jpg", ".jpeg", ".png" o ".gif".')
+    .withMessage('Tenes que cargar un archivo y debe ser de formato: ".jpg", ".jpeg", ".png" o ".gif".')
   ];
