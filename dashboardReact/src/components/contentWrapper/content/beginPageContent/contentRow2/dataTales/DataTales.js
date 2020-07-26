11ad0fb164d32c7446275c19c62f29aa93191898
Lucaster85 <lucasdc87@gmail.com>
2020-07-26 18:34:50 -0300

    refactorizacion y bootstrap al paginado

--- dashboardReact/src/components/contentWrapper/content/beginPageContent/contentRow2/dataTales/DataTales.js
+++ dashboardReact/src/components/contentWrapper/content/beginPageContent/contentRow2/dataTales/DataTales.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ColNames from './ColNames';
 import RowData from './RowData';
 
-
-
-
 export default class DataTales extends React.Component {
     constructor() {
         super();
@@ -17,12 +14,12 @@ export default class DataTales extends React.Component {
         };
     };
 
-
     apiCall(url, action) {
         fetch(url)
             .then(response => response.json())
             .then(action)
     };
+
     goFirstPage() {
         this.apiCall('http://localhost:3000/api/products', data => {
             let { first_page, next_page, prev_page } = data.pagination;
@@ -33,9 +30,8 @@ export default class DataTales extends React.Component {
                 prev_page,
                 total: data.meta.total
             })
-            console.log(next_page);
         });
-    }
+    };
 
     goNextPage() {
         this.apiCall(this.state.next_page, data => {
@@ -44,9 +40,10 @@ export default class DataTales extends React.Component {
                 courses: data.data,
                 next_page,
                 prev_page,
-            })
+            });
         });
-    }
+    };
+    
     goPrevPage() {
         this.apiCall(this.state.prev_page, data => {
             let { next_page, prev_page } = data.pagination;
@@ -57,39 +54,51 @@ export default class DataTales extends React.Component {
             })
         });
     }
+    
     componentDidMount() {
         this.goFirstPage();
-    }
+    };
 
     render() {
         let rowData = this.state.courses.map((course, i) => {
-            return <RowData key={i + course.name} name={course.name} price={course.price} description={course.description_short} professor={[course.professor.first_name, course.professor.last_name, course.professor.profession]}
-                category={course.category.title}
-                dates={[course.starts_date, course.ends_date]} vacancies={course.vacancies} />
+            return <RowData key={i + course.name} name={course.name} price={course.price} description={course.description_short}
+            professor={[course.professor.first_name, course.professor.last_name, course.professor.profession]}
+            category={course.category.title} dates={[course.starts_date, course.ends_date]} vacancies={course.vacancies} />
         })
-        let totalCourses = this.state.total;
+        let totalCourses = this.state.total,
 
-        let firstPage = <button onClick={() => this.goFirstPage()} >First Page</button>
+        firstPage = <button className="btn shadow bg-white border-left-primary" onClick={() => this.goFirstPage()} >First Page</button>,
+
+        prevButton;
+        this.state.prev_page != null ? prevButton = <button className="btn shadow bg-white border-left-success" onClick={() => this.goPrevPage()}> Prev 10 courses </button>
+        : prevButton = <button className="btn shadow bg-white border-left-success"> You are in the first page </button>
 
         let nextButton;
-        this.state.next_page != null ? nextButton = <button onClick={() => this.goNextPage()}> Next 10 courses </button> : nextButton = <button> You are in te last page </button>;
-
-        let prevButton;
-        this.state.prev_page != null ? prevButton = <button onClick={() => this.goPrevPage() }> Prev 10 courses </button> : prevButton = <button> You are in the first page </button>;
+        this.state.next_page != null ? nextButton = <button className="btn shadow bg-white border-left-warning" onClick={() => this.goNextPage()}> Next 10 courses </button>
+        : nextButton = <button className="btn shadow bg-white border-left-warning"> You are in te last page </button>
 
         return (
             <div>
-                <p>Total de Cursos: {` ${totalCourses}`}</p>
-                {firstPage}
-                {prevButton}
-                {nextButton}
+                <div className="container">
+                    <p>Total de Cursos: {` ${totalCourses}`}</p>
+                    <div className="row">
+                        <div className="col mb-3">
+                            {firstPage}
+                        </div>
+                        <div className="col mb-3">
+                            {prevButton}
+                        </div>
+                        <div className="col mb-3">
+                            {nextButton}
+                        </div>
+                    </div>
+                </div>
                 <div className="card sha1dow mb-4">
                     <div className="card-body">
                         <div className="table-responsive">
                             <table className="table table-bordered" id="dataTable" width="100%">
                                 <thead>
                                     <ColNames />
-
                                 </thead>
                                 <tfoot>
                                     <ColNames />
@@ -101,9 +110,20 @@ export default class DataTales extends React.Component {
                         </div>
                     </div>
                 </div>
-                {firstPage}
-                {prevButton}
-                {nextButton} 
+                <div className="container">
+                    <div className="row">
+                        <div className="col mb-3">
+                            {firstPage}
+                        </div>
+                        <div className="col mb-3">
+                            {prevButton}
+                        </div>
+                        <div className="col mb-3">
+                            {nextButton}
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
