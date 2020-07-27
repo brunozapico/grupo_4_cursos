ea4dead6b55435034a88951103f0054c85147734
brunozapico <brunoezapico@gmail.com>
2020-07-27 11:31:05 -0300

    Agregados API categories

--- WebAcademy/controllers/api/categoriesController.js
+++ WebAcademy/controllers/api/categoriesController.js
@@ -3,24 +3,29 @@ const db = require('../../database/models');
 module.exports = {
     list: (req, res) => {
         
-        let result = db.Category.findAll({
-            include: [{ association: 'courses' }]
-        })
-
-        let totalCourses = result
-
+        let result = db.Category.findAll({include: [{ association: 'courses' }]}),
+            totalCourses = result;
+        
         Promise.all([totalCourses, result])
             .then(([totalCourses, result]) => {
                 let categories = {
                     meta: {
                         status: 200,
+                        url: '/api/categories',
                         total: totalCourses.length
                     },
                     data: result
                 }
-                res.json(categories)
-            }
-            )
-            .catch(err => res.json(err));
+                res.status(200).json(categories)
+            })
+            .catch(error => {
+                res.status(400).json({
+                    status: "error",
+                    code: "400",
+                    message: "Bad Request",
+                    info: "Categories not found.",
+                    error: error
+                });
+            });
     },
 };
