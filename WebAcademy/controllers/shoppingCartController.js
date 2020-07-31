34c4ce49d35d562457adddcff5f72a2a6765f5f2
brunozapico <brunoezapico@gmail.com>
2020-07-31 12:03:49 -0300

    Funciona creacion de cart y course

--- WebAcademy/controllers/shoppingCartController.js
+++ WebAcademy/controllers/shoppingCartController.js
@@ -7,17 +7,20 @@ module.exports = {
         .then(cart => {
             if(!cart) {
                 // SI NO HAY CARRITO, LO CREO
-                let newCart = { user_id: req.params.userId };
-
-                db.ShoppingCart.create(newCart)
-                .then(cart => res.json(cart))
-                .catch(err => res.json({err, msg: 'ERROR'}));
+                db.ShoppingCart.create({user_id: req.params.userId})
+                .then(cart => {
+                    db.CartCourse.create({shopping_cart_id: cart.id, course_id: req.body.course_id})
+                    .then(course => res.json(course))
+                    .catch(err => res.json({msg: 'ERROR', err}));
+                })
+                .catch(err => res.json({msg: 'ERROR', err}));
             } else {
-                // SI YA EXISTIA, LO MUESTRO
-                return res.json({msg: 'YA EXISTIA', cart});
+                // SI YA EXISTIA, CREO EL CURSO EN ESE SHOPPING CART
+                db.CartCourse.create({shopping_cart_id: cart.id, course_id: Number(req.body.course_id)})
+                    .then(course => res.json(course))
+                    .catch(err => res.json({msg: 'ERROR', err}));
             };
-
         })
-        .catch(err => res.json({err, msg: 'ERROR'}));
+        .catch(err => res.json({msg: 'ERROR', err}));
     },
 };
