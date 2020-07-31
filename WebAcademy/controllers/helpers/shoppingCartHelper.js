110efcd4091c97ed3d7e07fac48032eea68dc92b
brunozapico <brunoezapico@gmail.com>
2020-07-31 12:10:27 -0300

    Creo helper para shoppingCart

---
+++ WebAcademy/controllers/helpers/shoppingCartHelper.js
@@ -0,0 +1,15 @@
+const db = require('../../database/models');
+
+module.exports = {
+    create_course: (cart_id, course_id, res) => {
+        db.CartCourse.create({
+                shopping_cart_id: cart_id,
+                course_id: course_id
+            })
+            .then(course => res.redirect('/'))
+            .catch(err => res.json({
+                msg: 'ERROR',
+                err
+            }));
+    },
+};
