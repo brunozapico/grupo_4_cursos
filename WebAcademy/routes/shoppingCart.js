a093e8f35ea87ea6c52b2af0161a863e878a54a2
brunozapico <brunoezapico@gmail.com>
2020-07-31 12:18:31 -0300

    Creo ruta para listar cursos del cart

--- WebAcademy/routes/shoppingCart.js
+++ WebAcademy/routes/shoppingCart.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const shoppingCartController = require('../controllers/shoppingCartController');
 
+router.get('/', shoppingCartController.list);
 router.post('/:userId', shoppingCartController.create);
 
 module.exports = router;
