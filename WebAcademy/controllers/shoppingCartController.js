c1233067db6a88040e56fe1b7271298253f7f344
brunozapico <brunoezapico@gmail.com>
2020-07-31 11:46:34 -0300

    Verifica carts, crear o mostrar

--- WebAcademy/controllers/shoppingCartController.js
+++ WebAcademy/controllers/shoppingCartController.js
@@ -2,6 +2,22 @@ const db = require('../database/models');
 
 module.exports = {
     create: (req, res, next) => {
-        res.send(`CURSO AGREGADO PARA EL USER: ${req.params.userId}`);
+        // BUSCO EL CARRITO ACTIVO DEL USUARIO
+        db.ShoppingCart.findOne({where: {user_id: req.params.userId, status: 1}})
+        .then(cart => {
+            if(!cart) {
+                // SI NO HAY CARRITO, LO CREO
+                let newCart = { user_id: req.params.userId };
+
+                db.ShoppingCart.create(newCart)
+                .then(cart => res.json(cart))
+                .catch(err => res.json({err, msg: 'ERROR'}));
+            } else {
+                // SI YA EXISTIA, LO MUESTRO
+                return res.json({msg: 'YA EXISTIA', cart});
+            };
+
+        })
+        .catch(err => res.json({err, msg: 'ERROR'}));
     },
 };
