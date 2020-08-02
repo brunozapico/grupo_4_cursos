e8b4b03ef5bd6d03fc222b19311d697768476386
brunozapico <brunoezapico@gmail.com>
2020-08-02 18:59:43 -0300

    send purchase mail helper

--- WebAcademy/controllers/helpers/mailerHelper.js
+++ WebAcademy/controllers/helpers/mailerHelper.js
@@ -2,7 +2,7 @@ const mailer = require('../../mailer/mailer');
 
 module.exports = {
     sendWelcomeEmail: (email_destination, user_name) => {
-        const mailOptions = {
+        let mailOptions = {
             from: 'no-reply@webacademy.com',
             to: email_destination,
             subject: '¡Bienvenido/a a Web Academy!',
@@ -18,6 +18,25 @@ module.exports = {
             if(err) { return console.log(err.message); };
 
             console.log(`Se ha enviado un email de verificación a: ${email_destination}.`);
+        });
+    },
+    sendPurchaseEmail: (email_destination) => {
+        let mailOptions = {
+            from: 'no-reply@webacademy.com',
+            to: email_destination,
+            subject: '¡Gracias por tu compra! - Web Academy',
+            html: 
+            `<h1 style="text-align: center">¡Te agredecemos por tu compra!</h1>
+            <h3 style="text-align: center">Somos Web Academy, la comunidad de educación en línea perfecta para vos.</h3>
+            <h4 style="text-align: center">¡Ahora, a estudiar!</h4>
+            
+            <hr>`
+        };
+
+        mailer.sendMail(mailOptions, function(err) {
+            if(err) { return console.log(err.message); };
+
+            console.log(`Se ha enviado un email verificando una nueva compra a: ${email_destination}.`);
         });
     },
 };
